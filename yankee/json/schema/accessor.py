74bbbec32b4ed61316d0ee0fca8f51900c177ed6
Parker Hancock <633163+parkerhancock@users.noreply.github.com>
2023-01-11 15:48:02 -0600

    Add support for JSONPath

--- yankee/json/schema/accessor.py
+++ yankee/json/schema/accessor.py
@@ -1,9 +1,20 @@
 from collections.abc import Sequence
 from yankee.util import camelize
 from yankee.base.accessor import do_nothing
+import jsonpath_ng
 
 
 def json_accessor(data_key, name, many, meta):
+    # Handle JSONPath objects passed as data keys
+    if isinstance(data_key, jsonpath_ng.JSONPath):
+        def accessor_func(obj):
+            result = [match.value for match in data_key.find(obj)]
+            try:
+                return result if many else result[0]
+            except IndexError:
+                return None
+        return accessor_func
+    # Handle no data key
     if data_key is False or (data_key is None and name is None):
         return do_nothing
     elif data_key is None:
@@ -21,3 +32,19 @@ def json_accessor(data_key, name, many, meta):
                 result = result.get(seg, None)
         return result
     return accessor_func
+
+def json_path_accessor(data_key, name, many, meta):
+    if isinstance(data_key, str):
+        try:
+            json_path = jsonpath_ng.parse(str)
+        except jsonpath_ng.exceptions.JsonPathParserError:
+            json_path = jsonpath_ng.ext.parse(str)
+        def accessor_func(obj):
+            result = [match.value for match in json_path.find(obj)]
+            try:
+                return result if many else result[0]
+            except IndexError:
+                return None
+        return accessor_func
+    else:
+        return json_accessor(data_key, name, many, meta)
