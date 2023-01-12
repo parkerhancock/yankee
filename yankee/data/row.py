c3928f1cfd4e9a10082c2a41427451f8d90e503f
Parker Hancock <633163+parkerhancock@users.noreply.github.com>
2023-01-12 10:22:09 -0600

    add to_mongo method to Row

--- yankee/data/row.py
+++ yankee/data/row.py
@@ -1,13 +1,22 @@
-import json
+import ujson as json
+import datetime
 from dataclasses import dataclass, asdict
 
 from .util import JsonEncoder
 from yankee.util import is_valid
 
+def convert_date(obj):
+    if isinstance(obj, datetime.date):
+        return datetime.datetime.combine(obj, datetime.datetime.min.time())
+    return obj
+
 @dataclass
 class Row():
     def to_dict(self):
         return asdict(self, dict_factory=lambda i: dict([(k, v) for k, v in i if is_valid(v)]))
+
+    def to_mongo(self):
+        return asdict(self, dict_factory=lambda i: dict([(k, convert_date(v)) for k, v in i if is_valid(v)]))
 
     def to_pandas(self):
         """Convert object to Pandas Series"""
